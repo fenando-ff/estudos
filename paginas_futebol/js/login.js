91d47be96e6030211f5512f0a0045fb743ee6696
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-10-02 20:27:19 -0300

    javaScript: base para autenticação de email e senha para fazer login

---
+++ paginas_futebol/js/login.js
@@ -0,0 +1,34 @@
+function login() {
+    let user = document.getElementById("email").value;
+    let password = document.getElementById("senha").value;
+    let aviso = document.getElementById("aviso");
+
+    let confUser = "fernando"
+    let confPassword = "fernando123"
+
+    switch (user, password) {
+        case user === confUser && password === confPassword:
+            aviso.style.color = "#2aff6d";
+            aviso.innerHTML = "Login realizado com sucesso ✅";
+            break;
+        case user === confUser && password !== confPassword:
+            aviso.style.color = "#ff4646ff";
+            aviso.innerHTML = "Senha não confere ❌";
+            break;
+        case user !== confUser && password === confPassword:
+            aviso.style.color = "#ff4646ff";
+            aviso.innerHTML = "Usuário não confere ❌";
+            break;
+        default:
+            if (user == "" || password == "") {
+                aviso.innerHTML = "Preencha todos os campos!";
+            } else if (user == "" && password == "") {
+                aviso.innerHTML = "Preencha todos os campos!";
+            } else {
+                aviso.style.color = "#ff4646ff";
+                aviso.innerHTML = "Acesso Negado ❌";
+            }
+            break;
+    };
+    
+};
