0df59ff9ebb7d9e21c6248139d3851f248efc85e
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-10-07 15:22:03 -0300

    Troquei o switch case por if e else para evitar futuros erros

--- paginas_futebol/js/login.js
+++ paginas_futebol/js/login.js
@@ -7,27 +7,21 @@ function login() {
     let confUser = "fernando"
     let confPassword = "fernando123"
 
-    switch (user, password) {
-        case user == confUser && password == confPassword:
-            aviso.style.color = "#2aff6d";
-            aviso.innerHTML = "Login realizado com sucesso ✅";
-            break;
-        case user === confUser && password !== confPassword:
-            aviso.style.color = "#ff4646ff";
-            aviso.innerHTML = "Senha não confere ❌";
-            break;
-        case user !== confUser && password === confPassword:
-            aviso.style.color = "#ff4646ff";
-            aviso.innerHTML = "Usuário não confere ❌";
-            break;
-        default:
-            if ((user == "" || password == "") || (user == "" && password == "")) {
-                aviso.innerHTML = "Preencha todos os campos!";
-            } else {
-                aviso.style.color = "#ff4646ff";
-                aviso.innerHTML = "Acesso Negado ❌";
-            }
-            break;
+    if (user == confUser && password == confPassword){
+        aviso.style.color = "#2aff6d";
+        aviso.innerHTML = "Login realizado com sucesso ✅";
+    } else if ((user == "" || password == "") || (user == "" && password == "")) {
+        aviso.style.color = "#fffc46ff";
+        aviso.innerHTML = "Preencha todos os campos!";
+    } else if (user === confUser && password !== confPassword) {
+        aviso.style.color = "#ff4646ff";
+        aviso.innerHTML = "Senha não confere ❌";
+    } else if (user !== confUser && password === confPassword){
+        aviso.style.color = "#ff4646ff";
+        aviso.innerHTML = "Usuário não confere ❌";
+    } else {
+        aviso.style.color = "#ff4646ff";
+        aviso.innerHTML = "Acesso Negado ❌";
     };
     
 };
