42b685c74eadad54ddc54bd46568b70580d2706c
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-10-09 17:31:52 -0300

    aula de js

---
+++ aulas/js/pesquisa.js
@@ -0,0 +1,11 @@
+
+
+function adicionarTarefa() {
+    let entrada = document.getElementById("digitaTarefa");
+    let texto = entrada.value;
+    let lista = document.getElementById("listaTarefa");
+
+    if(texto === "") {
+        
+    }
+}
