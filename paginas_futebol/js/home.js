1c5f3f400b3f51bb11bfb8f126543de2f06b5647
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-10-16 23:13:30 -0300

    Ajustando o funcionamento do carrossel de noticias

--- paginas_futebol/js/home.js
+++ paginas_futebol/js/home.js
@@ -1,8 +1,14 @@
-let ativo = document.querySelector(".ligado");
 let dots = document.querySelectorAll(".dot-carrossel");
 
 dots.forEach(dot => {
-    dot.addEventListener("click", ()=>{
-        dot.classList.add("ligado");
+    dot.addEventListener("click", () =>{
+        if(!dot.classList.contains("ligado")){
+            dots.forEach(ponto =>{
+                ponto.classList.remove("ligado");
+            });
+            dot.classList.add("ligado");
+        } else {
+            dot.classList.remove("ligado");
+        };
     });
 });
